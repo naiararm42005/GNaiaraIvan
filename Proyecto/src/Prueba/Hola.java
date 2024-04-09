0bfef67865f67405e6f0f51f465481a5868ee746
naiara <naiara@DESKTOP-99A3I0I>
2024-04-09 18:13:42 +0200

    metodo restar x primer usuario

--- Proyecto/src/Prueba/Hola.java
+++ Proyecto/src/Prueba/Hola.java
@@ -1,15 +1,16 @@
 package Prueba;
 
 public class Hola {
-	
-	
+	int restar(int a, int b) {
+		return a - b;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hola");
-System.out.println("ads");
-System.out.println("tercer cambio");
-System.out.println("cuarto cambio por remoto");
+		System.out.println("hola");
+		System.out.println("ads");
+		System.out.println("tercer cambio");
+		System.out.println("cuarto cambio por remoto");
 	}
 
 }
