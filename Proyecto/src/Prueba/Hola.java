bc0fa0cba5af65e285800eef2be01741bf121e51
WEB <WEB@A1EQUIPO12>
2024-04-10 08:53:35 +0200

    prueba commit

--- Proyecto/src/Prueba/Hola.java
+++ Proyecto/src/Prueba/Hola.java
@@ -3,7 +3,9 @@ package Prueba;
 public class Hola {
 	int restar(int a, int b) {
 		return a - b;
+		
 	}
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -11,6 +13,7 @@ public class Hola {
 		System.out.println("ads");
 		System.out.println("tercer cambio");
 		System.out.println("cuarto cambio por remoto");
+		System.out.println("prueba");
 	}
 
 }
